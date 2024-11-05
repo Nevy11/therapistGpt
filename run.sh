5e9b5492b56b5a85fdbf0a2cafe367a01cf08afb
nevy11 <smongare2003@gmail.com>
2024-11-05 20:07:53 +0300

    linux 'run.sh'

---
+++ run.sh
@@ -0,0 +1,6 @@
+echo "To run it type "sudo chmod u+x run.sh first""
+
+pip install -r requirements.txt
+
+echo "Congratulations your api is starting. Go to your front end and send the api's"
+python manage.py runserver
